7f7652343cf13bf2f2fda8b89dbb2aae582cd4ae
Lene Skov <lsko@itu.dk>
2017-10-10 10:23:07 +0200

    created empty mappers

---
+++ src/main/java/DBAccess/OrderMapper.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DBAccess;
+
+/**
+ *
+ * @author lene_
+ */
+public class OrderMapper {
+    
+    //Order getOrder(id)
+    
+    //List getAllOrders()
+    
+    //List getAllOrdersByUser(id)
+    
+    //create Order()
+    
+}
