1a4b503812b29be433784c240167061f5f2a6d55
Lene Skov <lsko@itu.dk>
2017-10-10 10:18:27 +0200

    added empty .jsp-pages and filled in dbconnector

--- src/main/java/DBAccess/DBConnector.java
+++ src/main/java/DBAccess/DBConnector.java
@@ -5,10 +5,24 @@
  */
 package DBAccess;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author lene_
  */
 public class DBConnector {
-    
+    private static String url = "jdbc:mysql://207.154.247.212:3306/legohus";
+    private static String username = "lsko";
+
+    private static String password = "LeneSkov";
+
+    public static Connection connection() throws ClassNotFoundException, SQLException {
+        
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(url, username, password);
+        return connection;
+    }
 }
---
+++ src/main/webapp/buildHouse.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : buildHouse
+    Created on : 10-10-2017, 10:16:35
+    Author     : lene_
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
