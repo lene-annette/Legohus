d8dd727b13b3391f6e65c115a8794c705427c9d1
Lene Skov <lsko@itu.dk>
2017-10-11 19:58:18 +0200

    fixed imports frontcontroller

--- src/main/java/Presentation/FrontController.java
+++ src/main/java/Presentation/FrontController.java
@@ -7,7 +7,6 @@ package Presentation;
 
 import Function.LegohusException;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
