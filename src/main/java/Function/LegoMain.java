b7e066ebcfa9a9401b9e964002f8df006d381be8
Lene Skov <lsko@itu.dk>
2017-10-11 13:34:11 +0200

    now login works

--- src/main/java/Function/LegoMain.java
+++ src/main/java/Function/LegoMain.java
@@ -15,28 +15,28 @@ import java.util.List;
 public class LegoMain {
       public static void main(String[] args) {
           
-          List<Integer> bricks = BuildingBricks.simpleBuilder(13, 9);
+//          List<Integer> bricks = BuildingBricks.simpleBuilder(13, 9);
+//          
+//          System.out.println("Længde 4x2: " + bricks.get(0));
+//          System.out.println("Længde 2x2: " + bricks.get(1));
+//          System.out.println("Længde 1x2: " + bricks.get(2));
+//          System.out.println("\nBredde 4x2: " +bricks.get(3));
+//          System.out.println("Bredde 2x2: " + bricks.get(4));
+//          System.out.println("Bredde 1x2: " + bricks.get(5));
+//          
+//          List<Integer> result = BuildingBricks.totalBricks(bricks, 4);
+//          System.out.println("\nAntal 4x2 i alt: " + (result.get(0) + result.get(3)));
+//          System.out.println("Antal 2x2 i alt: " + (result.get(1)+result.get(4)));
+//          System.out.println("Antal 1x2 i alt: " + (result.get(2)+result.get(5)));
           
-          System.out.println("Længde 4x2: " + bricks.get(0));
-          System.out.println("Længde 2x2: " + bricks.get(1));
-          System.out.println("Længde 1x2: " + bricks.get(2));
-          System.out.println("\nBredde 4x2: " +bricks.get(3));
-          System.out.println("Bredde 2x2: " + bricks.get(4));
-          System.out.println("Bredde 1x2: " + bricks.get(5));
           
-          List<Integer> result = BuildingBricks.totalBricks(bricks, 4);
-          System.out.println("\nAntal 4x2 i alt: " + (result.get(0) + result.get(3)));
-          System.out.println("Antal 2x2 i alt: " + (result.get(1)+result.get(4)));
-          System.out.println("Antal 1x2 i alt: " + (result.get(2)+result.get(5)));
-          
-          
-//        UserMapper um = new UserMapper();
-//        try{
-//            User user = um.login("lene@mail.dk","1234");
-//            System.out.println(user);
-//        }catch(LegohusException ex){
-//            System.out.println("message: " + ex.getMessage() );
-//        }
+        UserMapper um = new UserMapper();
+        try{
+            User user = um.login("lene@mail.dk","1234");
+            System.out.println(user);
+        }catch(LegohusException ex){
+            System.out.println("message: " + ex.getMessage() );
+        }
     }
     
 }
